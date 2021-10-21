bd54b34f36c79c36bcb6e7289ca7e0b3e1763360
Giovanni Malcolm <malcolm.giovanni@gmail.com>
2021-10-21 06:00:28 -0400

    generatemarkdown worked on. havent tested anything yet

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,56 +1,59 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-
-  if (!license){
-    return '';
-  }
-  else{
-
-
-
-    switch (operation) {
-      case 'sum':
-        console.log(maths.sum(numOne, numTwo));
-        break;
-      case 'difference':
-        console.log(maths.difference(numOne, numTwo));
-        break;
-      case 'product':
-        console.log(maths.product(numOne, numTwo));
-        break;
-      case 'quotient':
-        console.log(maths.quotient(numOne, numTwo));
-        break;
-      default:
-        console.log('Check your maths!');
+var renderLicenseBadge = (license) => {
+    switch (license) {
+      case 'MIT':
+        return '[![License](https://img.shields.io/badge/License-MIT-yellow.svg)]'
+      case 'APACHE 2.0':
+        return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]'
+      case 'GPL 3.0':
+        return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
+      case 'BSD 3':
+        return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]'
+      case 'None':
+        return ''
     }
-    
-
-
   }
 
-
-
-}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-
-
-
+var renderLicenseLink = (license) => {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT'
+    case 'APACHE 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0'
+    case 'GPL 3.0':
+      return 'https://opensource.org/licenses/gpl-3.0.html'
+    case 'BSD 3':
+      return 'https://opensource.org/licenses/BSD-3-Clause'
+    case 'None':
+      return ''
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+var renderLicenseSection = (license) => { 
+    switch (license) {
+      case 'MIT':
+        return renderLicenseBadge(license), renderLicenseLink(license)
+      case 'APACHE 2.0':
+        return renderLicenseBadge(license), renderLicenseLink(license)
+      case 'GPL 3.0':
+        return renderLicenseBadge(license), renderLicenseLink(license)
+      case 'BSD 3':
+        return renderLicenseBadge(license), renderLicenseLink(license)
+      case 'None':
+        return ''
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  
+
   return `# ${data.title}
-  //![Github License](licenseurl)
+  ${renderLicenseSection}
 
   ## Description
 
